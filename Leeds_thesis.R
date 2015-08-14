a6abe372961b242f190798d5bb4a24b966a6d579
Dylan W. Schwilk <dylan@schwilk.org>
2015-08-14 14:39:00 -0500

    DWS attempt at solution
    I do not know if I understood the problem correctly. I simply created a
    new difference column separately run for t_sun within each value of
    period.

--- Leeds_thesis.R
+++ Leeds_thesis.R
@@ -1,17 +1,31 @@
-pip_raw <- read.csv("desktop/leeds_pip.csv")
+# Needs comment header
 
-x <- pip_raw$t_sun
-y <- unique (pip_raw$period)
-UniqueVecDiff <- function (x, y) { # function UniqueVecDiff that takes two arguments x and y
-  as.factor(y) # set the vector y as a factor
-  for (i in 1: length (x)) { # for loop, stating for each value of x
-    if(unique(y) > 1){  # if the unique values of y are more than one row, since y is a factor 
-      xdiff <- diff(x, lag = 1, differences = 1) # calculates the differences between each value 
-      pip_diff <- cbind (pip_raw, xdiff)                            #in vector x
-    }
-  }
-  return (pip_diff) 
+# Iroro: "I need to write a function to subset observations from a data frame
+# based on two conditions on two vectors. The first condition is computing
+# iterative differences of one vector (time) if they fall within the same
+# unique values of another vector (time period). The second condition is to
+# subset observations (i.e. row values from all other vectors) within the data
+# frame for which the value of the time vector is < 3 secs."
+
+# Data reading and cleaning
+pip_raw <- read.csv("./leeds_pip.csv") # Use relative paths in scripts.
+pip_raw$period <- as.factor(pip_raw$period) 
+
+# DWS attempt. I do not know if I understood the problem correctly. I simply
+# created a new difference column separately run for t_sun within each value of
+# period.
+library(plyr)
+
+vecdiff <- function(x) {
+    xdiff <- diff(x, lag = 1, differences = 1)
+    # how to deal with first element? I'll add a zero. Maybe add a large number
+    # instead?
+    return(c(0, xdiff))
 }
 
-UniqueVecDiff(x, y)
+# are t_sun values sorted correctly within period?  I'm assuming they are.
 
+pip2 <- ddply(pip_raw, .(period), mutate, t_sun_diff = vecdiff(t_sun))
+pip_subset <- subset(pip2, t_sun_diff < 3) # but what about zeros added above?
+                                           # Do you want the first in series or
+                                           # not?
