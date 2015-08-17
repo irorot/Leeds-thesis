f9e9a891716630b31494c2180eaa859ad2175540
Dylan W. Schwilk <dylan@schwilk.org>
2015-08-17 16:06:43 -0500

    Sort df and use datetime objects

--- Leeds_thesis.R
+++ Leeds_thesis.R
@@ -7,28 +7,60 @@
 # subset observations (i.e. row values from all other vectors) within the data
 # frame for which the value of the time vector is < 3 secs."
 
-# Data reading and cleaning
-pip_raw <- read.csv("./leeds_pip.csv") # Use relative paths in scripts.
-pip_raw$period <- as.factor(pip_raw$period) 
-
-# DWS attempt. I do not know if I understood the problem correctly. I simply
-# created a new difference column separately run for t_sun within each value of
-# period.
+# packages
+library(lubridate)
 library(plyr)
 
+# Data reading and cleaning
+trans18test <- read.csv("./transect18_raw.csv")  # NEVER USE ABSOLUTE PATHS IN SCRIPTS!
+## datetrns <- dmy (trans18test$D500X_Date)
+## timetrns <- hms (trans18test$D500X_Time)
+## dat_tim <- paste(datetrns, timetrns) # R has now converted your datetime
+##                                      # objects to character vectors. See:
+## class(dat_tim)
+## > class(dat_tim)
+## [1] "character"
+
+#  Ok, lets create a clean date time STRING (character vector)
+
+trans18test$dt <- paste(trans18test$D500X_Date, trans18test$D500X_Time)
+# now convert that to a vector of datetime objects
+trans18test$dt <- dmy_hms(trans18test$dt)
+
+# Now you can always check that your object types are what you think. this is
+# iportant with times because R will always convert datetimes to string when
+# printing to the screen
+
+## > class(trans18test$dt)
+## [1] "POSIXct" "POSIXt" 
+## > 
+
+# Order our observations within sam_period and Class
+trans18test <- ddply(trans18test, .(Class, sam_period), function(x) x[order(x$dt), ])
+
+
 timediff <- function(x) {
-    # explanaton
-    if (length(x) == 1)  return(x)
-    xdiff <- diff(x, lag = 1, differences = 1)
-    # how to deal with first element? I'll add a zero. Maybe add a large number
-    # instead?
-    return(c(xdiff, 0)) # or c(xdiff, new_difftime(second=0))
+   #if (length(x) == 1)  return(4) # if a unique value of x occurs only once, retain 4 (being > 3)
+  # But 4 is not a datetime! That makes no sense.  I cannot understand your logic.
+  if (length(x) == 1)  return(new_difftime(second=4)) # don't you want the
+                                                      # actual time? Again, I
+                                                      # don't understand how
+                                                      # you can combine times
+                                                      # with differences, but
+                                                      # you have not explained
+                                                      # the data well enough
+                                                      # for me. I still don't
+                                                      # really get what you
+                                                      # intend to do with
+                                                      # groups for which there
+                                                      # is only one
+                                                      # measurement.
+  
+  xdiff <- diff(x, lag = 1, differences = 1)
+  return(c(xdiff, new_difftime(second=0)))  # does zero diff make sense for
+                                            # last measurement? I thought you
+                                            # did not want the last one?
 }
 
-# are t_sun values sorted correctly within period?  I'm assuming they are.
-
-pip2 <- ddply(pip_raw, .(period), mutate, t_sun_diff = timediff(t_sun))
-
-pip_subset <- subset(pip2, t_sun_diff < 3) # but what about zeros added above?
-                                           # Do you want the first in series or
-                                           # not?
+trans18run <- ddply(trans18test, .(Class, sam_period), mutate, dat_tim_diff = timediff(dt))
+trans18_subset <- subset(trans18run, dat_tim_diff > 3)
